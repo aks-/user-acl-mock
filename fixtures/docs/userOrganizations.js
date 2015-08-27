5398042c49cb477bb39fcf1dce13999df7c3f673
aks- <coderatlabs@gmail.com>
2015-08-27 20:02:55 +0530

    Add roles to userOrganizations doc

--- fixtures/docs/userOrganizations.js
+++ fixtures/docs/userOrganizations.js
@@ -3,27 +3,32 @@ module.exports = {
     {
       "user_id": "usernameOne",
       "organization_id": "npm",
-      "type": "user-organization"
+      "type": "user-organization",
+      "role": "owner"
     },
     {
       "user_id": "usernameOne",
       "organization_id": "krakenjs",
-      "type": "user-organization"
+      "type": "user-organization",
+      "role": "collaborator"
     },
     {
       "user_id": "usernameTwo",
       "organization_id": "OnlineBuddies",
-      "type": "user-organization"
+      "type": "user-organization",
+      "role": "collaborator"
     },
     {
       "user_id": "usernameTwo",
       "organization_id": "npm",
-      "type": "user-organization"
+      "type": "user-organization",
+      "role": "owner"
     },
     {
       "user_id": "usernameTwo",
       "organization_id": "krakenjs",
-      "type": "user-organization"
+      "type": "user-organization",
+      "role": "collaborator"
     },
   ]
 };
