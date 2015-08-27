42e02316ecf5e33ae90d0a9c012ed8619f0aa682
aks- <coderatlabs@gmail.com>
2015-08-27 16:05:27 +0530

    Add relationship docs

--- fixtures/docs/organizationPackages.js
+++ fixtures/docs/organizationPackages.js
@@ -1,22 +1,24 @@
-module.exports = [
-  {
-    "organization_id": "krakenjs",
-    "package_id": "adaro",
-    "type": "organization-package"
-  },
-  {
-    "organization_id": "krakenjs",
-    "package_id": "makara",
-    "type": "organization-package"
-  },
-  {
-    "organization_id": "npm",
-    "package_id": "npm",
-    "type": "organization-package"
-  },
-  {
-    "organization_id": "OnlineBuddies",
-    "package_id": "node-googlemaps",
-    "type": "organization-package"
-  }
-];
+module.exports = {
+  "docs": [
+    {
+      "organization_id": "krakenjs",
+      "package_id": "adaro",
+      "type": "organization-package"
+    },
+    {
+      "organization_id": "krakenjs",
+      "package_id": "makara",
+      "type": "organization-package"
+    },
+    {
+      "organization_id": "npm",
+      "package_id": "npm",
+      "type": "organization-package"
+    },
+    {
+      "organization_id": "OnlineBuddies",
+      "package_id": "node-googlemaps",
+      "type": "organization-package"
+    }
+  ]
+};
--- fixtures/docs/userOrganizations.js
+++ fixtures/docs/userOrganizations.js
@@ -1,27 +1,29 @@
-module.exports = [
-  {
-    "user_id": "usernameOne",
-    "organization_id": "npm",
-    "type": "user-organization"
-  },
-  {
-    "user_id": "usernameOne",
-    "organization_id": "krakenjs",
-    "type": "user-organization"
-  },
-  {
-    "user_id": "usernameTwo",
-    "organization_id": "OnlineBuddies",
-    "type": "user-organization"
-  },
-  {
-    "user_id": "usernameTwo",
-    "organization_id": "npm",
-    "type": "user-organization"
-  },
-  {
-    "user_id": "usernameTwo",
-    "organization_id": "krakenjs",
-    "type": "user-organization"
-  },
-];
+module.exports = {
+  "docs": [
+    {
+      "user_id": "usernameOne",
+      "organization_id": "npm",
+      "type": "user-organization"
+    },
+    {
+      "user_id": "usernameOne",
+      "organization_id": "krakenjs",
+      "type": "user-organization"
+    },
+    {
+      "user_id": "usernameTwo",
+      "organization_id": "OnlineBuddies",
+      "type": "user-organization"
+    },
+    {
+      "user_id": "usernameTwo",
+      "organization_id": "npm",
+      "type": "user-organization"
+    },
+    {
+      "user_id": "usernameTwo",
+      "organization_id": "krakenjs",
+      "type": "user-organization"
+    },
+  ]
+};
