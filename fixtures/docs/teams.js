923e5a334b1f4a4c4701b491e84044b6943c648b
aks- <coderatlabs@gmail.com>
2015-08-30 14:45:04 +0530

    Add permissions to teams per package

--- fixtures/docs/teams.js
+++ fixtures/docs/teams.js
@@ -4,36 +4,86 @@ module.exports = {
       "name": "Owners",
       "scope_id": "krakenjsOrganization",
       "users": ["usernameOne", "usernameFour"],
-      "packages": ["adaro", "makara", "npm"],
-      "type": "team"
+      "packages": {
+        "adaro": {
+          "permissions": ["read", "write"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }, 
+        "makara": {
+          "permissions": ["read", "write"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        },
+        "npm": {
+          "permissions": ["read", "write"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }
+      },
+      "type": "team",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "name": "Contributors",
       "scope_id": "krakenjsOrganization",
       "users": ["usernameOne", "usernameThree"],
-      "packages": ["makara"],
-      "type": "team"
+      "packages": {
+        "makara": {
+          "permissions": ["read"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }
+      },
+      "type": "team",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "name": "Boom",
       "scope_id": "npmOrganization",
       "users": ["usernameFour"],
-      "packages": ["npm"],
-      "type": "team"
+      "packages": {
+        "npm": {
+          "permissions": ["read"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }
+      },
+      "type": "team",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "name": "Beep",
       "scope_id": "npmOrganization",
       "users": ["usernameOne"],
-      "packages": ["npm"],
-      "type": "team"
+      "packages": {
+        "npm": {
+          "permissions": [],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }
+      },
+      "type": "team",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "name": "OneTeam",
       "scope_id": "OnlineBuddiesOrganization",
       "users": ["usernameOne", "usernameFour"],
-      "packages": ["node-googlemaps"],
-      "type": "team"
+      "packages": {
+        "node-googlemaps": {
+          "permissions": ["read", "write"],
+          "created": "2015-08-29T17:02:48.688Z",
+          "updated": "2015-08-29T17:02:48.688Z"
+        }
+      },
+      "type": "team",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
   ]
 };
