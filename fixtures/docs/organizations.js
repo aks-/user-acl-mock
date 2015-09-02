ea9d645db1b03f2ed2afe9a3a69622c1958bebd3
aks- <coderatlabs@gmail.com>
2015-09-03 00:05:01 +0530

    update fixtures

--- fixtures/docs/organizations.js
+++ fixtures/docs/organizations.js
@@ -5,21 +5,30 @@ module.exports = {
       "name": "npm",
       "description": "",
       "resource": "{}",
-      "type": "organization"
+      "pacakges": ["npm"],
+      "type": "organization",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "_id": "krakenjsOrganization",
       "name": "krakenjs",
       "description": "",
+      "pacakges": ["adaro", "makara"],
       "resource": "{}",
-      "type": "organization"
+      "type": "organization",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     },
     {
       "_id": "OnlineBuddiesOrganization",
       "name": "OnlineBuddies",
       "description": "",
+      "packages": ["node-googlemaps"],
       "resource": "{}",
-      "type": "organization"
+      "type": "organization",
+      "created": "2015-08-29T17:02:48.688Z",
+      "updated": "2015-08-29T17:02:48.688Z"
     }
   ]
 };
